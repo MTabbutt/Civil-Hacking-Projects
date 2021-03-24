0b7dc2da791c672fb2c36d4331f568e51c94b3b6
Megan Tabbutt <megan.tabbutt@gmail.com>
2021-03-24 11:23:05 -0500

    updating

--- BusSimProject/CensusDataWrapper.py
+++ BusSimProject/CensusDataWrapper.py
@@ -3,34 +3,17 @@ import censusdata
 class Census:
     
     def __init__(self, dataset, year):
-        self.dataset = dataset
-        self.year = year
+        self.geodatafile = "shapes.csv"
         
         return 
-        
-    def getSummary(self):
-        print("Dataset = ", self.dataset)
-        print("Year = ", self.year)
-        
+
+    
+# Pulling in setting the lat-lon data to be converted to counties list:
+
+    def set_geodatafile(self, filename):
+        self.geodatafile = filename
         return
     
-    
-    def getStateCodes(self, state='*'):
-        
-        try:
-            self.stateData = censusdata.geographies(censusdata.censusgeo([('state', '*')]), self.dataset, self.year)
-            if state == '*':
-                for key in self.stateData:
-                    print(key, " : ", self.stateData[key])
-            else:
-                print(state, " : ", self.stateData[state])
-            
-        except:
-            print("State was not found, use: getStateCodes(), to get a list of all states.")
-    
-        return 
-    
-    
-    
-    
+    def get_geodatafile(self, filename):
+        return self.geodatafile
     
